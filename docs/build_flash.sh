cfcedaa6f9ab360cfb405938a7e2d4222f3cdb23
kang <wahihi@naver.com>
2025-01-21 14:41:51 +0900

    ESP32 build & Download bash file
    1. ESP32-S3 target set...
    3. project clean & build...
    4. flash download & monitor...

---
+++ docs/build_flash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "ESP32-S3 build & flash start.."
+
+echo "1. ESP32-S3 target set..."
+idf.py set-target esp32s3
+
+# echo "2. menuconfig ..."
+# idf.py menuconfig
+
+echo "3. project clean & build..."
+#idf.py fullclean
+idf.py build
+
+echo "4. flash download & monitor..."
+idf.py -p /dev/ttyACM0 flash monitor
